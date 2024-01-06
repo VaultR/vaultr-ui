393b95ae5fb683fc67e474ff9d1818b13154fff2
wioletta.manka <wioletta.manka728@gmail.com>
2024-01-06 21:31:01 +0100

    Simplify getting note id from route

--- src/app/_components/editor/editor.component.ts
+++ src/app/_components/editor/editor.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {of, switchMap} from "rxjs";
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 import {CodemirrorBinding} from "y-codemirror";
 import * as Y from 'yjs';
 import {WebsocketProvider} from 'y-websocket';
@@ -14,25 +13,16 @@ import {environment} from "../../../environments/environment";
 })
 export class EditorComponent implements OnInit, OnDestroy {
   private binding: CodemirrorBinding | undefined;
-  public noteId: string | null = null;
   private provider: WebsocketProvider | undefined;
 
-  constructor(private activatedRoute: ActivatedRoute,
-              private router: Router) {
+  @Input() id: string | undefined;
+
+  constructor(private router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   public ngOnInit(): void {
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
 
-    this.activatedRoute.params.pipe(
-      switchMap(params => {
-        this.noteId = params['id'];
-        return of(this.noteId);
-        // return this.noteService.getNoteDetails(this.noteId);
-      })
-    ).subscribe(noteDetails => {
-
-    });
   }
 
   public ngOnDestroy(): void {
@@ -42,12 +32,9 @@ export class EditorComponent implements OnInit, OnDestroy {
 
   public onCodeMirrorLoaded(codeMirrorComponent: any): void {
     const document = new Y.Doc();
-    this.provider = new WebsocketProvider(
-      `ws://${environment.noteApiUrl}`,
-      this.noteId!,
-      document
+    this.provider = new WebsocketProvider(`ws://${environment.noteApiUrl}`, this.id!, document
     );
-    const text = document.getText(this.noteId!);
+    const text = document.getText(this.id!);
     const undoManager = new Y.UndoManager(text);
 
     this.binding = new CodemirrorBinding(text, codeMirrorComponent.codeMirror, this.provider.awareness, {
